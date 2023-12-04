14242db4a9ede50b9bdd31d04e48c489b89df715
drzuthar <149124359+drzuthar@users.noreply.github.com>
2023-12-04 10:18:38 -0600

    Add files via upload

---
+++ school project/school project/school_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  school_projectApp.swift
+//  school project
+//
+//  Created by Jacob Olivar Silva on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct school_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
